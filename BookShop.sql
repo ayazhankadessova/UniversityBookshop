041bf1fee5a09c0088a9bc7c9f477f08f3a73eee
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-03-31 13:29:11 +0800

    Update BookShop.sql

--- BookShop.sql
+++ BookShop.sql
@@ -34,6 +34,7 @@ CREATE TABLE OrdersNew (
     FOREIGN KEY (student_id) REFERENCES Student(student_id)
 );
 
+
 CREATE TABLE Orders_Book (
     order_id INT,
     book_id INT,
@@ -44,11 +45,11 @@ CREATE TABLE Orders_Book (
     FOREIGN KEY (book_id) REFERENCES Book(book_id)
 );
 
+-- book alr exists
 
 ALTER TABLE Orders_Book
 ADD CONSTRAINT FK_ORDERS_BOOK_ORDER_ID
 FOREIGN KEY (order_id) REFERENCES OrdersNew(order_id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED NULL;
-
 
 COMMIT;
 
@@ -60,6 +61,29 @@ INSERT INTO Book VALUES (3, 'Pride and Prejudice', 'Jane Austen', 8.99, 40);
 INSERT INTO Book VALUES (4, 'The Great Gatsby', 'F. Scott Fitzgerald', 11.99, 20);
 INSERT INTO Book VALUES (5, 'One Hundred Years of Solitude', 'Gabriel García Márquez', 9.99, 60);
 
+-- Insert into Student table
+PROMPT INSERT Student TABLE;
+INSERT INTO Student VALUES (1, 'John Smith', 'Male', 'English', 50, 0.05);
+INSERT INTO Student VALUES (2, 'Sarah Johnson', 'Female', 'Biology', 75, 0.10);
+INSERT INTO Student VALUES (3, 'David Chen', 'Male', 'Computer Science', 100, 0.15);
+INSERT INTO Student VALUES (4, 'Emily Wong', 'Female', 'History', 25, 0.02);
+
+-- Insert into OrdersNew table
+PROMPT INSERT OrdersNew TABLE;
+INSERT INTO OrdersNew VALUES (1, 1, '2023-03-29', 40.00, 'Credit Card', '1234567890123456');
+INSERT INTO OrdersNew VALUES (2, 3, '2023-03-30', 30.00, 'Debit Card', '6543210987654321');
+INSERT INTO OrdersNew VALUES (3, 2, '2023-03-30', 50.00, 'Credit Card', '9876543210123456');
+INSERT INTO OrdersNew VALUES (4, 4, '2023-03-31', 20.00, 'PayPal', NULL);
+
+
+-- Insert into Orders_Book table
+PROMPT INSERT Orders_Book TABLE;
+INSERT INTO Orders_Book VALUES (1, 1, 2, '2023-03-02');
+INSERT INTO Orders_Book VALUES (1, 3, 1, '2023-03-02');
+INSERT INTO Orders_Book VALUES (2, 2, 3, '2023-03-01');
+INSERT INTO Orders_Book VALUES (3, 4, 2, '2023-03-03');
+INSERT INTO Orders_Book VALUES (4, 1, 1, '2023-03-01');
+INSERT INTO Orders_Book VALUES (4, 2, 1, '2023-03-01');
 
 -- COMMIT;
 
